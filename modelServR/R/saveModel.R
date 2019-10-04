41cb45226a882815cdaaca81f428f2a1d30403c4
famulare <mikefamulare@gmail.com>
2019-10-04 17:30:16 +0000

    fixed bug in quoting introduced in 19e91ccd650a86189192aba6630e24f4d4dfed05

--- modelServR/R/saveModel.R
+++ modelServR/R/saveModel.R
@@ -180,7 +180,7 @@ saveModel <- function(model, modelStoreDir =  Sys.getenv('MODEL_STORE', '/home/r
     # write to our model db file
     write.table(
       newRow, file = modelDBfilename, sep = '\t', row.names = FALSE, col.names = !file.exists(modelDBfilename),
-      quote = TRUE, append = file.exists(modelDBfilename)
+      quote = FALSE, append = file.exists(modelDBfilename)
     )
   }
   
@@ -212,7 +212,7 @@ saveModel <- function(model, modelStoreDir =  Sys.getenv('MODEL_STORE', '/home/r
     model$modeledData,
     paste(modelStoreDir, '/', filename, '.csv', sep = ''),
     row.names = FALSE,
-    quote = FALSE
+    quote = TRUE
   )
   write.table(
     newRow, file = modelDBfilename, sep = '\t', row.names = FALSE, col.names = !file.exists(modelDBfilename),
