469375395657736397cebf73d136e6cde79c83d3
famulare <mikefamulare@gmail.com>
2019-10-29 16:44:03 +0000

    many changes to make forecast_ILI more compatible with rest of codebase - replaced non-tidyverse data manipulation packages with tidy equivalents - added explicit namespace references where not clear - added newly required packages to Dockerfile.RBuildEnv - converted all week variable to iso 8601 standard - used same pattern for forecast week extension as in expandDB.R - made all references to current date for forecasting explicit instead of implicit differences
    There are still things that should be further cleaned up for stability and reuse.  Such as:
    - make references to WA_ILI configurable.
    - more descriptive variable names
    - integration into dbViewR workflow for appending ILI to data.
    - ...

--- incidenceMapR/R/forecast_ILI.R
+++ incidenceMapR/R/forecast_ILI.R
@@ -11,9 +11,8 @@
 #' @param outpath where to save
 #'  
 #' @import ggplot2
-#' @import reshape2
-#' @import plyr
 #' @import dplyr
+#' @import tidyr
 #' @import gridExtra
 #' @import tibble
 #'
@@ -22,11 +21,11 @@
 #' plot ILI data and forecast
 #'   plotforecasts(dat_WAILI, "ILI", "ARIMA forecast ILI", "ARIMA_fc_ILI")
  
-plotforecasts<- function(dat, fitted_param, ggtitlestr, outpath){
+plotforecasts<- function(dat, fitted_param , ggtitlestr = NULL , outpath = NULL){
   
   fc <- dat %>% select(matches("fc_|upper_|lower_")) %>% colnames() %>% c(fitted_param)
   bounds_only <- dat %>% select(matches("upper_|lower_")) %>% colnames()
-  data_toplot<- select(dat, "encountered_week", fc) %>% melt(id.vars=c("encountered_week", bounds_only))
+  data_toplot<- select(dat, "encountered_week", fc) %>% tidyr::gather(variable, value, -encountered_week, -lower_95, -upper_95)
   
   #everything in its own facet except the ILI which is overlaid on all facets
   all_otherdata <- data_toplot[-grep(paste("^", fitted_param, "$", sep=""), data_toplot$variable), ]
@@ -38,8 +37,13 @@ plotforecasts<- function(dat, fitted_param, ggtitlestr, outpath){
     scale_color_manual(labels = c("fc", fitted_param), values = c("red", "gray")) + 
     ylab(fitted_param) + ggtitle(ggtitlestr) + 
     geom_ribbon(aes(ymin=lower_95, ymax=upper_95, fill="95% CI", group=variable),  alpha=0.2)
+ 
   
-  ggsave(paste(outpath, ".pdf", sep=""), plot=p, width=7, height=5, units="in")
+  if(!is.null(outpath)){
+    ggsave(paste(outpath, ".png", sep=""), plot=p, width=7, height=5, units="in")
+  } else{
+    print(p) 
+  }
 }
 
 
@@ -53,9 +57,8 @@ plotforecasts<- function(dat, fitted_param, ggtitlestr, outpath){
 #' @param num_prior_data number of previous data points (weeks) to use to fit the ARIMA model
 #' @param num_future_data number of weeks forward to make ILI estimates
 #' 
-#' @import MMWRweek
+#' @import lubridate
 #' @import reshape2
-#' @import plyr
 #' @import dplyr
 #' @import lubridate
 #' @import forecast
@@ -63,32 +66,53 @@ plotforecasts<- function(dat, fitted_param, ggtitlestr, outpath){
 #' @import boot
 #' 
 #' @return ILI_estimate - dataframe with point estimate and 95% CI upper and lower for num_future_weeks
-calcfc<- function(dat, num_prior_data=52, num_future_data = 1){
+calcfc<- function(dat, num_prior_data=52, currentWeek=currentWeek){
   
   
   #take logit of ILI data
-  dat$ILI<-logit(dat$ILI)
+  dat$ILI<-boot::logit(dat$ILI)
   
-  #calculate the extra_dates for the forecast_weeks (TODO fix when it crosses to year 2020)
-  last_week<-dat$week_start[nrow(dat)]
-  extra_dates_day<-last_week + weeks(1:num_future_data)
-  extra_dates <- paste(MMWRweek(extra_dates_day)[,1],"-W",
-                                sprintf("%02d",MMWRweek(extra_dates_day)[,2]), sep="") %>% data.frame()#add CDC week number
-  
-  colnames(extra_dates)<-c("encountered_week")  
-  
-  
-  #set up data frame to store forecasts
-  fc_master <- slice(dat, 1:n()) %>% select(encountered_week)
-  end_fc_time<-nrow(dat)-1
-  if(!is.null(extra_dates))
-  {
-    fc_master<-rbind(fc_master, extra_dates[1])
-    end_fc_time <- end_fc_time + 1
+  # regularize weeks with 0 reported ILI
+  idx <- is.finite(dat$ILI)
+  dILI <- median(abs(diff(dat$ILI[idx])))
+  for(k in which(!idx)){
+    if (k>1){
+      dat$ILI[k] <- dat$ILI[k-1]-dILI
+    } else {
+      dat$ILI[k] <- dat$ILI[k+1]-dILI
+    }
   }
   
-  #column names to add 
-  fc_master<-add_column(fc_master, fc = NA, upper = NA, lower=NA)
+  
+  ## calculate forecast date range
+  currentWeek <- paste(lubridate::isoyear(Sys.time()) ,'-W',lubridate::isoweek(Sys.time()),sep='')
+  last_ILI_week <- max(dat$encountered_week)
+  
+  minYear <- year<-as.numeric(gsub('-W[0-9]{2}','',last_ILI_week))
+  maxYear <- year<-as.numeric(gsub('-W[0-9]{2}','',currentWeek))
+  minWeek <- as.numeric(gsub('[0-9]{4}-W','',last_ILI_week ))
+  maxWeek <- as.numeric(gsub('[0-9]{4}-W','',currentWeek )) + (maxYear-minYear)*52 
+  
+  weeks <- 1+( (seq(minWeek+1,maxWeek,by=1)-1) %% 52)
+  yearBreaks <- c(0,which(diff(weeks)<1), length(weeks))
+  years=c()
+  for (k in 2:length(yearBreaks)){
+    years <- c(years, rep(minYear+(k-2), yearBreaks[k]-yearBreaks[k-1]  ))
+  }
+  
+  forecast_weeks <- paste(years,'-W',sprintf("%02d",weeks),sep='')
+
+  #set up data frame to store forecasts
+  fc_master <- slice(dat, 1:n()) %>% select(encountered_week) 
+  end_fc_time <- nrow(fc_master)
+  
+  if(!is.null(forecast_weeks)){
+    fc_master <- fc_master %>% tibble::add_row(encountered_week = forecast_weeks)
+  }
+  fc_master <- fc_master %>% tibble::add_column(fc = NA, upper = NA, lower=NA)
+  
+  num_future_data <- nrow(fc_master) - end_fc_time
+
   
   #run forecasting for each week + future forecast
   for (current_time in (num_prior_data+1):end_fc_time){
@@ -99,14 +123,14 @@ calcfc<- function(dat, num_prior_data=52, num_future_data = 1){
     #fit model and forecast 1 wk ahead for all weeks
     fit <- auto.arima(dat_s$ILI) 
     fc <- forecast(fit, h=1) #forecast 1 wk ahead
-    if (current_time == nrow(dat) && !is.null(extra_dates)){
+    if (current_time == nrow(dat) && !is.null(forecast_weeks)){
       fc <- forecast(fit, h=num_future_data) #forecast more weeks ahead for last time point
     }
     
     
     #merge fitted value with original data for plotting 
     fc_master_timerange <- (current_time+1):(current_time+1)
-    if(current_time == nrow(dat) && !is.null(extra_dates)){
+    if(current_time == nrow(dat) && !is.null(forecast_weeks)){
       fc_master_timerange <- (current_time+1):(current_time+num_future_data)
     }
     
@@ -116,14 +140,14 @@ calcfc<- function(dat, num_prior_data=52, num_future_data = 1){
     fc_master$lower[fc_master_timerange] <- data.matrix(fc$lower[,2])
     
     #inverse logit the forecast and the range 
-    fc_master$fc[fc_master_timerange] <-inv.logit(fc_master$fc[fc_master_timerange])
-    fc_master$upper[fc_master_timerange] <-inv.logit(fc_master$upper[fc_master_timerange])
-    fc_master$lower[fc_master_timerange] <-inv.logit(fc_master$lower[fc_master_timerange])
+    fc_master$fc[fc_master_timerange] <-boot::inv.logit(fc_master$fc[fc_master_timerange])
+    fc_master$upper[fc_master_timerange] <-boot::inv.logit(fc_master$upper[fc_master_timerange])
+    fc_master$lower[fc_master_timerange] <-boot::inv.logit(fc_master$lower[fc_master_timerange])
     
     
   }
   #rename columns
-  colnames(fc_master) <- c("week", "fc_ILI", "upper_95", "lower_95")
+  colnames(fc_master) <- c("encountered_week", "fc_ILI", "upper_95", "lower_95")
   
   return(fc_master)
 }
@@ -131,15 +155,17 @@ calcfc<- function(dat, num_prior_data=52, num_future_data = 1){
 
 
 
-#' 
+#' forecast_ILI: function to extend CDC ILI data to desired week via ARIMA forecast
 #' 
 #' @param num_prior_data number of previous data points to use to fit the ARIMA model
 #' @param num_future_data number of weeks forward from the latest observation in the CDC database to make ILI estimates
 #' @return ILI_estimate - dataframe with point estimate and 95% CI upper and lower for num_future_data weeks
 #' 
 #' @import cdcfluview
-#' @import MMWRweek
 #' @import boot
+#' @import magrittr
+#' @import dplyr
+#' @import lubridate
 #'
 #' @export
 #' @examples
@@ -147,29 +173,31 @@ calcfc<- function(dat, num_prior_data=52, num_future_data = 1){
 #'    ILI_forecast <- forecast_ILI(num_prior_data=52, num_future_weeks = 2)
 #'    
 #'    
-forecast_ILI <- function(num_prior_data=52, num_future_weeks = 2){
+forecast_ILI <- function(num_prior_data=52, currentWeek = NULL){
     
+  # current week being run
+  if( is.null(currentWeek)){
+    currentWeek <- paste(lubridate::isoyear(Sys.time()) ,'-W',lubridate::isoweek(Sys.time()),sep='')
+  }
+  
   #load the ILI Data
-  ILI<- ilinet(region=c("state"), years = c(2017, 2018, 2019)) #all states for 2018-19 flu season
+  ILI<- cdcfluview::ilinet(region=c("state"), years = 2017:(lubridate::isoyear(Sys.time())+1)) #all states through this season
 
   #get only WA rows and unweighted ILI columns 
-  WA_ILI<- ILI %>% filter(grepl('Washington', region)) %>% select(unweighted_ili, week_start) %>% dplyr::rename(ILI = unweighted_ili) 
-  #add CDC week number
-  WA_ILI$encountered_week <- paste(MMWRweek(WA_ILI$week_start)[,1],"-W",
-                                sprintf("%02d",MMWRweek(WA_ILI$week_start)[,2]), sep="")#add CDC week number 
+  WA_ILI<- ILI %>% filter(grepl('Washington', region)) %>% select(unweighted_ili, week_start) %>% dplyr::rename(ILI = unweighted_ili)
+  
+  # add isoweek as ordered factor
+  WA_ILI$encountered_week <- paste(lubridate::isoyear(WA_ILI$week_start) ,'-W',sprintf("%02d",lubridate::isoweek(WA_ILI$week_start)),sep='')
+  WA_ILI$encountered_week <- factor(WA_ILI$encountered_week, levels=sort(unique(WA_ILI$encountered_week)), ordered = TRUE)
   
   #convert ILI to fraction btwn 0 and 1
   WA_ILI$ILI <- WA_ILI$ILI/100
   
+
   #forecast WA ILI 1 wk ahead for weeks where ILI data is known using ARIMA
   #combine ILI data with forecast ILI data
-  dat_WAILI<-left_join(calcfc(WA_ILI, num_prior_data=num_prior_data, num_future_data=num_future_weeks),
-                       WA_ILI, 
-                      by = c("week" = "encountered_week" )) 
-  dat_WAILI<-dplyr::rename(dat_WAILI, encountered_week=week)
-  
-  #optional save a plot of the ILI fit and  forecast
-  plotforecasts(dat_WAILI, "ILI", "ARIMA forecast ILI", "ARIMA_fc_ILI")
-  
+  dat_WAILI<-left_join(calcfc(WA_ILI, num_prior_data=num_prior_data, currentWeek=currentWeek),
+                       WA_ILI,by = 'encountered_week') 
+
   return(dat_WAILI)
 }
