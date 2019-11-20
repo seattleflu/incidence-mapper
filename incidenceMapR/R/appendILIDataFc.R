ab4a5d6c407a119cdff23673719895797b10e3ef
famulare <mikefamulare@gmail.com>
2019-11-20 02:18:42 +0000

    modified appendILIDataFc to remove work better as a covariate - append logit of ILI - hackily handle zeros that go to -inf in logit
    Currently, the ILI covariate is not useful, but I will rerun when data updates.

--- incidenceMapR/R/appendILIDataFc.R
+++ incidenceMapR/R/appendILIDataFc.R
@@ -4,6 +4,7 @@
 #' @param currentWeek current week
 #' 
 #' @import dplyr
+#' @import boot
 #' 
 #' @return db with added ILI Data + fc tibble
 #' 
@@ -15,9 +16,18 @@ appendILIDataFc <- function(db, currentWeek){
   
   #ILI_forecast_dat includes the actual past ILI values and the forecasted data for the past year up to currentWeek
   
-  #reduce the ILI_forecast_dat so that it returns actual ILI values if available, and only the forecast data for the recent weeks where actual ILI unavailable
+  # reduce the ILI_forecast_dat so that it returns actual ILI values if available,
+  # and only the forecast data for the recent weeks where actual ILI unavailable
   ILI_forecast_dat$ILI_all <- dplyr::if_else(is.na(ILI_forecast_dat$ILI), ILI_forecast_dat$fc_ILI, ILI_forecast_dat$ILI)
+  
+  # remove zeros with linear interpolation (hack because zeros break things later)
+  zeroIdx <- ILI_forecast_dat$ILI_all==0
+  ILI_forecast_dat$ILI_all[zeroIdx] <- 1/2*ILI_forecast_dat$ILI[which(zeroIdx)-1] + 1/2*ILI_forecast_dat$ILI[which(zeroIdx)+1]
+  
   ILI_forecast_dat <- ILI_forecast_dat %>% dplyr::select(encountered_week, ILI_all) %>% dplyr::rename(ILI = ILI_all)
+  
+  # logit for covariate
+  ILI_forecast_dat$ILI <- boot::logit(ILI_forecast_dat$ILI)
   
   #add to the db
   db$observedData <- dplyr::left_join(db$observedData, ILI_forecast_dat, by = c("encountered_week"="encountered_week"))
