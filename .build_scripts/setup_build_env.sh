c7bf5cb43094759a6ee313544ce5dbe17958cc71
Clinton Collins <devclinton@users.noreply.github.com>
2019-12-10 13:51:48 -0800

    Update setup_build_env.sh

--- .build_scripts/setup_build_env.sh
+++ .build_scripts/setup_build_env.sh
@@ -1,2 +1,2 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 docker login idm-docker-staging.packages.idmod.org/sfim_build_env:latest --username "${bamboo_UserArtifactory}" --password "${bamboo_PasswordArtifactory@Q}"
