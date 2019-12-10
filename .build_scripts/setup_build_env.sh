8ba35f0e1ce3f06c7725f51e27cc19efa1cbf68e
Clinton Collins <devclinton@users.noreply.github.com>
2019-12-10 13:54:18 -0800

    Update setup_build_env.sh

--- .build_scripts/setup_build_env.sh
+++ .build_scripts/setup_build_env.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker login idm-docker-staging.packages.idmod.org --username "${bamboo_UserArtifactory}" --password "${bamboo_PasswordArtifactory@Q}"
+docker login idm-docker-staging.packages.idmod.org --username "${bamboo_UserArtifactory}" --password "${bamboo_PasswordArtifactory}"
